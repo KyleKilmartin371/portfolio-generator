bcf6d8df3db9dd01b67761421232bca0adc8e0a1
KyleKilmartin371 <kylekilmartin@yahoo.com>
2021-08-11 17:09:02 -0700

    generate webpage

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-const printProfileData = (profileDataArr) =>{
-    for (let i = 0; i < profileDataArr.length; i+= 1) {
-        console.log(profileDataArr[i]); 
-    }
+const generatePage = require('./src/page-template.js');
 
+const profileDataArgs = process.argv.slice(2);
 
-console.log('===============');
-
-profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const [name, github] = profileDataArgs;
 
 
-printProfileData(profileDataArgs);
+  fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
+  
